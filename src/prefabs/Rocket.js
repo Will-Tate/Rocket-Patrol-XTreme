bd64db675b8463825ccfa92016f7adbb12940ae5
wbtate <tatew@ucsc.edu>
2022-04-11 14:02:42 -0700

    Finally working

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,33 @@
+const { Phaser } = require("../../lib/phaser");
+
+// Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+      super(scene, x, y, texture, frame);
+  
+      // add object to existing scene
+      scene.add.existing(this);
+      this.isFiring=false;
+      this.moveSpeed=2;
+    }
+
+    update(){
+        if(!this.isFiring){
+            if(keyLEFT.isDown && this.x>=borderUISize+this.width){
+                this.x-=this.moveSpeed;
+            } else if(keyRIGHT.isDown && this.x <=game.config.width-borderUISize-this.width){
+                this.x+=this.moveSpeed;
+            }
+        }
+        if(Phaser.Input.Keyboard.JustDown(keyF)){
+            this.isFiring=true;
+        }
+        if(this.isFiring&&this.y>=borderUISize*3+borderPadding){
+            this.y-=this.moveSpeed;
+        }
+        if(this.y<=borderUISize*3+borderPadding){
+            this.isFiring=false;
+            this.y=game.config.height-borderUISize-borderPadding;
+        }
+    }
+  }
